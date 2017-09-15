2bcefd9da90f998d123879b0a9a84f8e746b4433
antonstrand <jag@antonstrand.se>
2017-09-15 22:47:45 +0200

    attachments has to be an array with an object

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.post('/fredag', function (req, res, next) {
   const userName = req.body.user_name
 
   const botPayload = {
-    attachments: {
+    attachments: [{
       fallback: 'Visar om det är fredag.',
       title: 'Är det fredag?',
       title_link: 'http://erefredag.se/',
       text: answer,
       color: rColor
-    }
+    }]
   }
   // Loop otherwise..
   if (userName !== 'slackbot') {
